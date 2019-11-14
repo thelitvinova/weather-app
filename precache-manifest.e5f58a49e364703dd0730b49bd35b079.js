633d40cd659f3bc7bd88f04cdb5f034a9c942eb5
Aleksandra Litvinova <aleksandra@MacBook-Pro-Aleksandra.local>
2019-11-14 19:08:22 +0700

    Updates

---
+++ precache-manifest.e5f58a49e364703dd0730b49bd35b079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6bd1ba4242e088dd15894b7e98dafe",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ba07ef307a3ea17a25e0",
+    "url": "/weather-app/static/css/main.67a165f3.chunk.css"
+  },
+  {
+    "revision": "d6d7468bd64d0de64341",
+    "url": "/weather-app/static/js/2.e46bfe79.chunk.js"
+  },
+  {
+    "revision": "ba07ef307a3ea17a25e0",
+    "url": "/weather-app/static/js/main.93167567.chunk.js"
+  },
+  {
+    "revision": "d22b54445391e22b0f71",
+    "url": "/weather-app/static/js/runtime~main.fdd590f4.js"
+  },
+  {
+    "revision": "3675ff25a3e499dbadc2d1be55296832",
+    "url": "/weather-app/static/media/weather.3675ff25.jpg"
+  }
+]);
